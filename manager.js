c580931ff277abeaeb4591ca5e3a45ab7df7c586
Jose Viteri <Jose@Joses-MBP.home>
2018-05-11 21:08:18 -0400

    updated logic

--- manager.js
+++ manager.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root", //Your username
-    password: "", //Your password
+    password: "password", //Your password
     database: "The-store"
 })
 
--- seeds.sql
+++ seeds.sql
@@ -15,4 +15,19 @@ VALUES ("LulzBot Mini Desktop 3D Printer", "Industrial and Scientific", 1250,5);
 INSERT INTO products (product_name, department_name, price,stock_quantity)
 VALUES ("LulzBot PolyLite PLA Polymaker Filament", "Industrial and Scientific", 25,15);
 
+INSERT INTO products (product_name, department_name, price,stock_quantity)
+VALUES ("Adafruit NeoPixel Digital RGB LED Strip ", "Electronics", 21.14,18);
+
+INSERT INTO products (product_name, department_name, price,stock_quantity)
+VALUES ("Head and Shoulders Classic Clean 2 in 1 Anti Dandruff Shampoo","Beauty & Personal Care", 15.64,100);
+
+INSERT INTO products (product_name, department_name, price,stock_quantity)
+VALUES ("Power Strip Tower JACKYLED Surge Protector Electric Charging Station ", "Electronics", 24.99,35);
+
+INSERT INTO products (product_name, department_name, price,stock_quantity)
+VALUES ("Loctite Super Glue Gel", "Tools & Home Improvement", 5.04,450);
+
+INSERT INTO products (product_name, department_name, price,stock_quantity)
+VALUES ("Levoit Kana Himalayan Salt Lamp ", "Tools & Home Improvement", 25.99,16);
+
 SELECT * FROM products;
